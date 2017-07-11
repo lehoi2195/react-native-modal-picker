a11da47d1f953a294870424d007853ce5e68d1bf
lehoi2195 <lehoi2195@gmail.com>
2017-07-11 14:28:48 +0700

    Update style.js

--- style.js
+++ style.js
@@ -1,182 +1,82 @@
 'use strict';
 
-import React,{
-    PropTypes
-} from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
 
-import {
-    View,
-    StyleSheet,
-    Dimensions,
-    Modal,
-    Text,
-    ScrollView,
-    TouchableOpacity,
-    Platform
-} from 'react-native';
+const {height, width} = Dimensions.get('window');
 
-import styles from './style';
-import BaseComponent from './BaseComponent';
+const PADDING = 8;
+const BORDER_RADIUS = 4;
+const FONT_SIZE = 16;
+const HIGHLIGHT_COLOR = 'rgba(0,118,255,0.9)';
+const OPTION_CONTAINER_HEIGHT = 175;
 
-let componentIndex = 0;
+export default StyleSheet.create({
 
-const propTypes = {
-    data: PropTypes.array,
-    onChange: PropTypes.func,
-    initValue: PropTypes.string,
-    style: View.propTypes.style,
-    selectStyle: View.propTypes.style,
-    optionStyle: View.propTypes.style,
-    optionTextStyle: Text.propTypes.style,
-    sectionStyle: View.propTypes.style,
-    sectionTextStyle: Text.propTypes.style,
-    cancelStyle: View.propTypes.style,
-    cancelTextStyle: Text.propTypes.style,
-    overlayStyle: View.propTypes.style,
-    cancelText: PropTypes.string
-};
+    overlayStyle: {
+        width: width,
+        height: height,
+        backgroundColor: 'rgba(0,0,0,0.7)'
+    },
 
-const defaultProps = {
-    data: [],
-    onChange: ()=> {},
-    initValue: 'Select me!',
-    style: {},
-    selectStyle: {},
-    optionStyle: {},
-    optionTextStyle: {},
-    sectionStyle: {},
-    sectionTextStyle: {},
-    cancelStyle: {},
-    cancelTextStyle: {},
-    overlayStyle: {},
-    cancelText: 'Cancel'
-};
+    optionContainer: {
+        borderRadius:BORDER_RADIUS,
+        width:width*0.8,
+        height:OPTION_CONTAINER_HEIGHT,
+        backgroundColor:'rgba(255,255,255,0.8)',
+        left:width*0.1,
+        top:(height-OPTION_CONTAINER_HEIGHT)/2
+    },
 
-export default class ModalPicker extends BaseComponent {
+    cancelContainer: {
+        left:width*0.1,
+        top:(height-OPTION_CONTAINER_HEIGHT)/2 + 10
+    },
 
-    constructor() {
+    selectStyle: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 13,
+        borderRadius: BORDER_RADIUS
+    },
 
-        super();
+    selectTextStyle: {
+        color: 'red',
+        fontSize: FONT_SIZE
+    },
 
-        this._bind(
-            'onChange',
-            'open',
-            'close',
-            'renderChildren'
-        );
+    cancelStyle: {
+        borderRadius: BORDER_RADIUS,
+        width: width * 0.8,
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        padding: PADDING
+    },
 
-        this.state = {
-            animationType: 'slide',
-            modalVisible: false,
-            transparent: false,
-            selected: 'please select'
-        };
+    cancelTextStyle: {
+        textAlign: 'center',
+        color: '#333',
+        fontSize: FONT_SIZE
+    },
+
+    optionStyle: {
+        padding: PADDING,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+    },
+
+    optionTextStyle: {
+        textAlign: 'center',
+        fontSize: FONT_SIZE,
+        color: HIGHLIGHT_COLOR
+    },
+
+    sectionStyle: {
+        padding: PADDING * 2,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+    },
+
+    sectionTextStyle: {
+        textAlign: 'center',
+        fontSize: FONT_SIZE
     }
-
-    componentDidMount() {
-        this.setState({selected: this.props.initValue});
-        this.setState({cancelText: this.props.cancelText});
-    }
-
-    componentWillReceiveProps(nextProps) {
-      if (nextProps.initValue != this.props.initValue) {
-        this.setState({selected: nextProps.initValue});
-      }
-    }
-
-    onChange(item) {
-        this.props.onChange(item);
-        this.setState({selected: item.label});
-        this.close();
-    }
-
-    close() {
-      this.setState({
-        modalVisible: false
-      });
-    }
-
-    open() {
-      this.setState({
-        modalVisible: true
-      });
-    }
-
-    renderSection(section) {
-        return (
-            <View key={section.key} style={[styles.sectionStyle,this.props.sectionStyle]}>
-                <Text style={[styles.sectionTextStyle,this.props.sectionTextStyle]}>{section.label}</Text>
-            </View>
-        );
-    }
-    renderOption(option) {
-        return (
-            <TouchableOpacity key={option.key} onPress={()=>this.onChange(option)}>
-                <View style={[styles.optionStyle, this.props.optionStyle]}>
-                    <Text style={[styles.optionTextStyle,this.props.optionTextStyle]}>{option.label}</Text>
-                </View>
-            </TouchableOpacity>)
-    }
-
-    renderOptionList() {
-        var options = this.props.data.map((item) => {
-            if (item.section) {
-                return this.renderSection(item);
-            } else {
-                return this.renderOption(item);
-            }
-        });
-
-        return (
-            <View style={[styles.overlayStyle, this.props.overlayStyle]} key={'modalPicker'+(componentIndex++)}>
-                <View style={styles.optionContainer}>
-                    <ScrollView keyboardShouldPersistTaps = {'always'}>
-                        <View style={{paddingHorizontal:10}}>
-                            {options}
-                        </View>
-                    </ScrollView>
-                </View>
-                <View style={styles.cancelContainer}>
-                    <TouchableOpacity onPress={this.close}>
-                        <View style={[styles.cancelStyle, this.props.cancelStyle]}>
-                            <Text style={[styles.cancelTextStyle,this.props.cancelTextStyle]}>{this.props.cancelText}</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-
-            </View>);
-    }
-
-    renderChildren() {
-
-        if(this.props.children) {
-            return this.props.children;
-        }
-        return (
-            <View style={[styles.selectStyle, this.props.selectStyle]}>
-                <Text style={[styles.selectTextStyle, this.props.selectTextStyle]}>{this.state.selected}</Text>
-            </View>
-        );
-    }
-
-    render() {
-
-        const dp = (
-          <Modal transparent={true} ref="modal" visible={this.state.modalVisible} onRequestClose={this.close} animationType={this.state.animationType}>
-          {this.renderOptionList()}
-          </Modal>
-        );
-
-        return (
-            <View style={this.props.style}>
-                {dp}
-                <TouchableOpacity onPress={this.open}>
-                    {this.renderChildren()}
-                </TouchableOpacity>
-            </View>
-        );
-    }
-}
-
-ModalPicker.propTypes = propTypes;
-ModalPicker.defaultProps = defaultProps;
+});
